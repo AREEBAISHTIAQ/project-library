55e81c9e9adb3989c24f53514f1754a263c5d1f3
AREEBAISHTIAQ <areeba.aic@gmail.com>
2018-11-17 20:03:22 +0500

    added basics

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 let myLibrary = [];
 
-function Click() {
-    let newtitle = prompt("Enter the name of the book, please");
-    let newauthor = prompt("Enter the name of the author, please");
-    document.getElementById("showtitle").innerText = newtitle;
-    document.getElementById("showauthor").innerText = newauthor;
+var modal = document.getElementById('form');
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
+
+function Submit() {
+    let submit = document.getElementById("submit").addEventListener("click", function() {
+        document.getElementById("showtitle").innerHTML = document.getElementById("title").value;
+        document.getElementById("showauthor").innerHTML = document.getElementById("author").value;
+        document.getElementById("showpages").innerHTML = document.getElementById("pages").value;
+        //document.getElementById("showread").innerHTML = document.getElementById("read").value;
+
+        document.getElementById("form").style.display='none';
+    });
+}
+
+function Radio(read) {
+    document.getElementById("showread").value = read;
+} 
+
 function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-}
-
-console.log(Book())
-
-function addBookToLibrary() {
-
 }
